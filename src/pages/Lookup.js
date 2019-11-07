29215bad56fd161a0b061868db41c278893e5a15
Cohen <dcoco91@gmail.com>
2019-11-07 14:59:54 -0500

    edited the setstate to only grab the first list of synonyms so it would correctly display the words. Will need to fix to grab and modify all elements later. Added a key prop to remove console error

--- src/pages/Lookup.js
+++ src/pages/Lookup.js
@@ -3,9 +3,9 @@ import Button from '../comps/Button';
 import API from '../utils/API';
 import Word from '../comps/Word';
 
-class Lookup extends React.Component{
+class Lookup extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             word: '',
@@ -14,65 +14,67 @@ class Lookup extends React.Component{
             syns: [],
             ants: []
         }
-        
+
     }
     handleClick = e => {
-        e.preventDefault ();
-        
+        e.preventDefault();
+
         API.getSyn(this.state.word).then(resp => {
 
             // if we only get an array of strings, the word is too small.
-            if(typeof(resp.data[0])  === 'string'){
+            if (typeof (resp.data[0]) === 'string') {
 
                 // something for the user here
                 console.log('Word too small, try a longer one')
-            } else{
+            } else {
                 console.log(resp)
                 console.log(resp.data[0].meta.syns)
                 this.setState({
                     lookup: true,
                     data: resp.data,
-                    syns: resp.data[0].meta.syns
+                    syns: resp.data[0].meta.syns[0]
                 })
+
 
             }
         }).catch(err => {
             console.log(err)
         })
 
-        
-        
-    } 
+
+
+    }
 
 
     handleChange = event => {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        this.setState ({
-          [name]: value,
+        this.setState({
+            [name]: value,
         });
-        
-      };
-    render(){
-        const theSyns = this.state.syns.map(item => (
-            <Word syn={true} word={item} />
-        ))
-        return(
+
+    };
+    render() {
+        const theSyns = this.state.syns.map(item => {
+            return <Word syn={true} word={item} key={item} />
+        });
+
+        return (
             <div>
                 <h1>Synonym Finder</h1>
                 <div className='card'>
                     <form>
-                        <input 
-                        type='text'
-                        value={this.state.value}
-                        name='word'
-                        onChange={this.handleChange} />
-                    <Button onClick={this.handleClick} />
+                        <input
+                            type='text'
+                            value={this.state.value}
+                            name='word'
+                            onChange={this.handleChange} />
+                        <Button onClick={this.handleClick} />
                     </form>
                     <div className='container-fluid'>
                         <div className='d-flex flex-column'>
-                        {theSyns}
+                            {theSyns}
                         </div>
                     </div>
                 </div>
@@ -80,9 +82,9 @@ class Lookup extends React.Component{
         )
     }
 
-    
 
-    
+
+
 }
 
 export default Lookup;
