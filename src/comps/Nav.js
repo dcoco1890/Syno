941a30f6505d56f8d8526a29fe7ff8ccd7943038
Cohen <dcoco91@gmail.com>
2019-11-14 21:02:42 -0500

    style changes to the nav bar to make it prettier. Next is the totalcard and synantlist

--- src/comps/Nav.js
+++ src/comps/Nav.js
@@ -5,19 +5,23 @@ export default function Nav(){
     const styles = {
         ul: {
             display: 'flex',
-            justifyContent: 'space-around',
             listStyle: 'none',
             boxShadow: '1px 1px 5px rgba(0,0,0,0.6)',
             marginBottom: '2.5rem',
-            padding: '.5rem 0'
+            padding: 0,
+            background: 'linear-gradient(10deg, rgba(253,29,146,1) 0%, rgba(252,139,69,1) 100%)'
+        },
+        li: {
+            fontSize: '1.1rem',
+            padding: '.9rem 1rem',
         }
     }
     return(
         <div>
         <nav>
             <ul style={styles.ul}>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/lookup'>Lookup</Link></li>
+                <Link to='/'><li style={styles.li}>Home</li></Link>
+                <Link to='/lookup'><li style={styles.li}>Lookup</li></Link>
             </ul>
         </nav>
 
